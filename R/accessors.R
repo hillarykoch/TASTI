62508a7a71aac55787fc313d0bdb2b0c80d122c0
Hillary Koch <hillary.koch01@gmail.com>
2018-03-16 17:45:00 -0400

    add function for file reading

--- R/accessors.R
+++ R/accessors.R
@@ -23,3 +23,43 @@ get_mles <- function(fits){
     }
     mles
 }
+
+
+get_triples <- function(file, branchLengths = TRUE){
+    trees <- read_table(file, col_names = FALSE)
+    taxa <- read.tree(text = trees$X1[1])$tip.label
+    splits <- str_split(trees$X1, pattern = "")
+
+    input <- matrix(rep(0, 5*nrow(trees)), ncol = 5)
+    colnames(input) <- c(taxa, "t1", "t2")
+    for(i in 1:nrow(trees)){
+        test <- splits[[i]]
+        if(all(test[1:2] == c("(", "("))){
+            commaIdx <- grep(test, pattern = ",")
+            colonIdx <- grep(test, pattern = ":")
+
+            outgroup <- paste0(test[(commaIdx[2]+1):(colonIdx[4]-1)], collapse = "")
+            t1 <- as.numeric(paste0(test[(colonIdx[1]+1):(commaIdx[1]-1)], collapse = ""))
+            t2 <- as.numeric(paste0(test[(colonIdx[3]+1):(commaIdx[2]-1)], collapse = ""))
+        } else{
+            commaIdx <- grep(test, pattern = ",")
+            colonIdx <- grep(test, pattern = ":")
+            parenIdx <- grep(test, pattern = "\\(")
+
+            outgroup <- paste0(test[(parenIdx[1]+1):(colonIdx[1]-1)], collapse = "")
+            t1 <- as.numeric(paste0(test[(colonIdx[2]+1):(commaIdx[2]-1)], collapse = ""))
+            t2 <- as.numeric(paste0(test[(colonIdx[1]+1):(commaIdx[1]-1)], collapse = ""))
+        }
+
+        input[i,outgroup] <- 1
+        input[i,"t1"] <- t1
+        input[i,"t2"] <- t2
+    }
+
+    if(branchLengths){
+        input
+    } else{
+        colSums(input)[1:3]
+    }
+}
+
